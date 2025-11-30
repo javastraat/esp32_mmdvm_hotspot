b518aacbf31a84e65f41d5b97a4db58cd8fb99cb
Albert Einstein <javastraat@hotmail.com>
2025-11-30 18:39:35 +0100

    move irmware files

--- make-beta.sh
+++ make-beta.sh
@@ -1,8 +1,8 @@
 rm -R build
 arduino-cli compile --fqbn esp32:esp32:esp32 . --export-binaries --verbose      
-cp build/esp32.esp32.esp32/esp32_mmdvm_hotspot.ino.bin ./update_beta.bin
+cp build/esp32.esp32.esp32/esp32_mmdvm_hotspot.ino.bin ./firmware/update_beta.bin
 rm -R build
-git add update_beta.bin
+git add firmware/update_beta.bin
 git commit -m "Updated update file (update_beta.bin)"
 git push
 
--- make-bin.sh
+++ make-bin.sh
@@ -1,8 +1,8 @@
 rm -R build
 arduino-cli compile --fqbn esp32:esp32:esp32 . --export-binaries --verbose      
-cp build/esp32.esp32.esp32/esp32_mmdvm_hotspot.ino.bin ./update.bin
+cp build/esp32.esp32.esp32/esp32_mmdvm_hotspot.ino.bin ./firmware/update.bin
 rm -R build
-git add update.bin
+git add firmware/update.bin
 git commit -m "Updated update file (update.bin)"
 git push
 
