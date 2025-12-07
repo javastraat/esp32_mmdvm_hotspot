07d2d2ef156f54eb214088ba55dd194b04a102d1
Albert Einstein <javastraat@hotmail.com>
2025-12-07 11:45:27 +0100

    fixed s3 make script

--- make-beta-s3.sh
+++ make-beta-s3.sh
@@ -1,5 +1,5 @@
-rm -R build
-arduino-cli compile --fqbn esp32:esp32:esp32s3 . --export-binaries --verbose      
+rm -R build 2>/dev/null || true
+arduino-cli compile --fqbn esp32:esp32:esp32s3:CDCOnBoot=cdc,CPUFreq=240,DebugLevel=none,DFUOnBoot=default,EraseFlash=none,EventsCore=1,FlashMode=qio,FlashSize=16M,JTAGAdapter=default,LoopCore=1,MSCOnBoot=default,PartitionScheme=app3M_fat9M_16MB,PSRAM=opi,UploadMode=default,UploadSpeed=921600,USBMode=hwcdc,ZigbeeMode=default . --export-binaries --verbose      
 cp build/esp32.esp32.esp32s3/esp32_mmdvm_hotspot.ino.bin ./firmware/update_beta.bin
 rm -R build
 git add firmware/update_beta.bin
