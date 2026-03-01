0461b4f3d5dad6fd8b6652ef2bf87a64ecd226cf
Albert Einstein <javastraat@hotmail.com>
2026-03-01 13:41:15 +0100

    clean directory

--- include/secrets.h
+++
@@ -1,4 +0,0 @@
-#define WIFI_PASSWORD    "itoldyoualready"
-#define WIFI_AP_PASSWORD "mmdvm1234"
-#define WEB_PASSWORD     "pi-star"
-#define DMR_PASSWORD     "passw0rd"
--- test-pocsag.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST http://esp32-mmdvm.local/api/send-pocsag  -d "ric=2041152&message=Hello1&encoding=alpha"
-curl -X POST http://esp32-mmdvm.local/api/send-pocsag  -d "ric=2041152&message=Hello2&encoding=alpha"
-curl -X POST http://esp32-mmdvm.local/api/send-pocsag  -d "ric=2041152&message=Hello3&encoding=alpha"
-curl -X POST http://esp32-mmdvm.local/api/send-pocsag  -d "ric=2041152&message=Hello4&encoding=alpha"
-
